d5538ed87afc7b492cc15a77c8d7d3f24f174b6b
Mark Andrei Encanto <markandrei.encanto@wvsu.edu.ph>
2024-09-24 06:31:28 +0800

    removed carousel sliding

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -15,16 +15,6 @@ interface CarouselProps {
 const Carousel: React.FC<CarouselProps> = ({ items }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? items.length - 1 : prevIndex - 1
-    );
-  };
-
   const goToSlide = (index: number) => {
     setCurrentIndex(index);
   };
