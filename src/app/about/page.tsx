fa3035ed08a0a35f6a7a653f0cf1d0ef71b7a781
Mark Andrei Encanto <markandrei.encanto@wvsu.edu.ph>
2024-09-26 14:13:01 +0800

    add animations in about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,57 @@
+"use client";
+import { motion } from "framer-motion";
+
+const teamMembers = [
+  {
+    name: "Mark Andrei Encanto",
+    role: "Project Manager",
+    imgSrc: "/assets/MARKPIC.png",
+    imgPosition: "-top-10",
+    imgWidth: "w-[240px]",
+    imgHeight: "h-[423px]",
+  },
+  {
+    name: "Chariz Dianne Falco",
+    role: "Developer",
+    imgSrc: "/assets/CHAPIC.png",
+    imgPosition: "-top-[6rem]",
+    imgWidth: "w-[260px]",
+    imgHeight: "h-[480px]",
+  },
+  {
+    name: "Khee Jay Galvez",
+    role: "Developer",
+    imgSrc: "/assets/KJPIC.png",
+    imgPosition: "-top-[8.5rem]",
+    imgWidth: "w-[360px]",
+    imgHeight: "h-[520px]",
+  },
+  {
+    name: "Allan Andrews Tacuel",
+    role: "Illustrator/Asset Director",
+    imgSrc: "/assets/allanp.png",
+    imgPosition: "-top-[4.6rem]",
+    imgWidth: "w-[1080px]",
+    imgHeight: "h-[480px]",
+  },
+  {
+    name: "Cherilyn Marie Deocampo",
+    role: "UI/UX Designer",
+    imgSrc: "/assets/CHEPIC.png",
+    imgPosition: "bottom-[11.25rem]",
+    imgWidth: "w-[340px]",
+    imgHeight: "h-[520px]",
+  },
+  {
+    name: "Jephone Israel Jireh Torre",
+    role: "UI/UX Designer",
+    imgSrc: "/assets/JEPHPIC.png",
+    imgPosition: "bottom-[11.25rem]",
+    imgWidth: "w-[460px]",
+    imgHeight: "h-[620px]",
+  },
+];
+
 export default function AboutPage() {
   return (
     <div>
@@ -14,97 +68,75 @@ export default function AboutPage() {
       >
         <div className="mx-16 pt-[15%] tracking-wider">
           <div className="flex items-center">
-            <div className="w-4 h-9 bg-[#620808] mr-4"></div>
-            <h1 className="text-[4rem] font-extrabold">What is Kantonize?</h1>
+            <motion.div
+              className="w-4 h-9 bg-[#620808] mr-4"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.7 }}
+            ></motion.div>
+            <motion.h1
+              className="text-[4rem] font-extrabold"
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.5 }}
+            >
+              What is Kantonize?
+            </motion.h1>
           </div>
-          <h2 className="ml-[3rem] tracking-wider text-[16px]">
+          <motion.h2
+            className="ml-[3rem] tracking-wider text-[16px]"
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0 }}
+          >
             Kantonize is a Kanton Customizing Website. The customization you
             never knew you’ll need.
-          </h2>
+          </motion.h2>
         </div>
       </div>
 
       <div className="text-center my-12">
-        <h1 className="text-6xl font-extrabold">MEET THE TEAM</h1>
+        <motion.h1
+          className="text-6xl font-extrabold"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.7 }}
+        >
+          MEET THE TEAM
+        </motion.h1>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 h-auto py-16 relative mt-28">
-        <div className="flex flex-col items-center justify-center  relative">
-          <div className="bg-[#912828] w-full max-w-lg aspect-[4/3] relative"></div>
-          <img
-            src="/assets/MARKPIC.png"
-            alt="Person 1"
-            className="absolute -top-10 w-[240x] h-[423px] z-10"
-          />
-          <div className="text-center mb-32">
-            <h2 className="text-black text-xl font-bold">
-              Mark Andrei Encanto
-            </h2>
-            <p className="text-gray-600">Project Manager</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 h-auto py-16 relative mt-36">
+        {teamMembers.map((member, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center relative"
+          >
+            <motion.div
+              className="bg-[#912828] w-full max-w-lg aspect-[4/3] relative"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.4 }}
+            ></motion.div>
+            <motion.img
+              src={member.imgSrc}
+              alt={member.name}
+              className={`absolute ${member.imgPosition} ${member.imgWidth} ${member.imgHeight} z-10`}
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.6 }}
+            />
+            <motion.div
+              className="text-center mb-32"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 1 }}
+            >
+              <h2 className="text-black text-xl font-bold">{member.name}</h2>
+              <p className="text-gray-600">{member.role}</p>
+            </motion.div>
           </div>
-          <div className="bg-[#912828] w-full max-w-lg aspect-[4/3]"></div>
-          <img
-            src="/assets/KJPIC.png"
-            alt="Person 1"
-            className="absolute top-[26.6rem] w-[360px] h-[520px] z-10"
-          />
-          <div className="text-center mb-32">
-            <h2 className="text-black text-xl font-bold">Khee Jay Galvez</h2>
-            <p className="text-gray-600">Developer</p>
-          </div>
-          <div className="bg-[#912828] w-full max-w-lg aspect-[4/3]"></div>
-          <img
-            src="/assets/CHEPIC.png"
-            alt="Person 1"
-            className="absolute bottom-[11.25rem] w-[340px] h-[520px] z-10"
-          />
-          <div className="text-center mb-32">
-            <h2 className="text-black text-xl font-bold">
-              Cherilyn Marie Deocampo
-            </h2>
-            <p className="text-gray-600">UI/UX Designer</p>
-          </div>
-        </div>
-
-        {/* Second grid item */}
-        <div className="flex flex-col items-center justify-center relative">
-          <div className="bg-[#912828] w-full max-w-lg aspect-[4/3] relative"></div>
-          <img
-            src="/assets/CHAPIC.png"
-            alt="Person 2"
-            className="absolute -top-[8.5rem] w-[240x] h-[520PX] z-10"
-          />
-          <div className="text-center mb-32">
-            <h2 className="text-black text-xl font-bold">
-              Chariz Dianne Falco
-            </h2>
-            <p className="text-gray-600">Developer</p>
-          </div>
-          <div className="bg-[#912828] w-full max-w-lg aspect-[4/3]"></div>
-          <img
-            src="/assets/allanp.png"
-            alt="Person 2"
-            className="absolute top-[30.5rem] w-[1080px] h-[480PX] z-10"
-          />
-          <div className="text-center mb-32">
-            <h2 className="text-black text-xl font-bold">
-              Allan Andrews Tacuel
-            </h2>
-            <p className="text-gray-600">Illustrator/Asset Director</p>
-          </div>
-          <div className="bg-[#912828] w-full max-w-lg aspect-[4/3]"></div>
-          <img
-            src="/assets/JEPHPIC.png"
-            alt="Person 2"
-            className="absolute bottom-[11.25rem] w-[460px] h-[620px] z-10"
-          />
-          <div className="text-center mb-32">
-            <h2 className="text-black text-xl font-bold">
-              Jephone Israel Jireh Torre
-            </h2>
-            <p className="text-gray-600">UI/UX Designer</p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
